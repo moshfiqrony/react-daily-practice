c98ab5dd5b42778cab08279e4955d82fe1f824ee
Md. Moshfiqur Rahman Rony <15203012@iubat.edu>
2022-02-09 17:35:51 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import Textfield from "@mui/material/TextField";
-import { useRef } from "react";
-
 const App = () => {
-  const inputEl = useRef();
-  const handleClick = () => {
-    // find existing value's length
-    const len = inputEl.current.value?.length;
-    // update the new value
-    inputEl.current.value = inputEl.current.value + " New value";
-    // Select the old value from srart
-    inputEl.current.setSelectionRange(0, len);
-    // Focus the input
-    inputEl.current.focus();
-  };
-  return (
-    <div>
-      <div>App</div>
-      <Textfield inputRef={inputEl} multiline minRows={5} />
-      <button onClick={handleClick}>Suggest prompt</button>
-    </div>
-  );
+  return <h1>App</h1>;
 };
 
 export default App;
